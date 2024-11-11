dcc2a36b0a85d4eca479256b642c7d9966656875
Pinank Soni <rs.pinanksoni@gmail.com>
2024-11-11 13:48:47 +0530

    merge solve

--- app/Http/Controllers/Api/v1/InquiryDetailsController.php
+++ app/Http/Controllers/Api/v1/InquiryDetailsController.php
@@ -6,32 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\InquiryDetails;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
-<<<<<<< HEAD
-=======
 use Illuminate\Support\Facades\Validator;
->>>>>>> development
 
 class InquiryDetailsController extends Controller
 {
-    public function addInquiry12(Request $request)
-    {
-
-        $data = $request->all();
-        $data['created_by'] = 1;
-        $data['vehicle_no'] = strtoupper($request->vehicle_no);
-        $lastInquiryId = InquiryDetails::orderBy('id', 'desc')->first()->id ?? 0;
-        $data['inquiry_no'] = 'INQ-' . $lastInquiryId + 1; // If no records, start with 1
-
-        $inquirySave = InquiryDetails::create($data);
-        if ($this->isNotNullOrEmptyOrZero($inquirySave)) {
-
-            $inquiryId = $inquirySave->inquiry_no;
-            return $this->successResponse([], "Inquiry No # $inquiryId added  Successfully, We will contact you soon.");
-        } else {
-            return $this->failResponse([], 'Something Wrong');
-        }
-    }
-
     public function addInquiry(Request $request)
     {
         // Define validation rules
@@ -49,21 +27,15 @@ class InquiryDetailsController extends Controller
             'mobile.digits' => 'Mobile number must be 10 digits long.'
         ];
 
-        // Perform validation
         $validator = Validator::make($request->all(), $rules, $messages);
 
-        // Check if validation fails
         if ($validator->fails()) {
-            // Return custom fail response with validation errors
             return $this->failResponse([], 'Validation Error Please enter valid Vehicle Number,Mobile number', $validator->errors());
         }
 
-        // Process data if validation passes
         $data = $request->all();
         $data['created_by'] = 1;
         $data['vehicle_no'] = strtoupper($request->vehicle_no);
-
-        // Generate inquiry number
         $lastInquiryId = InquiryDetails::orderBy('id', 'desc')->first()->id ?? 0;
         $data['inquiry_no'] = 'INQ-' . ($lastInquiryId + 1);
 
